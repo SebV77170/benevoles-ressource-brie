a75fd3fd3b67f7dcd783d29ed726ae1f90fad114
sonaa212 <bhargaviramanadane1@gmail.com>
2023-03-01 10:35:18 +0000

    Replace compta.php

--- public/compta.php
+++ public/compta.php
@@ -8,15 +8,10 @@ require '../actions/users/userdefinition.php';
 
 entete('Comptabilité bénévoles','Comptabilité bénévoles','4');
  
- if(isset($_POST['ok'])):
-  foreach($_POST['habilitation'] as $k=>$v):
-      $admin->updateHabilitation((int)$k,(int)$v);
-  endforeach;
-endif;
- 
+
  
 if(isset($admin)):
-$listOfUsers = $admin->getAllUsersAndDateWaiting();
+$listOfUsers = $admin->getNbtotalbenevolat();
 
 
 
@@ -41,30 +36,8 @@ $listOfUsers = $admin->getAllUsersAndDateWaiting();
                     <th scope="col"><?=$v['nom']?></th>
                     <th scope="col"><?=$v['prenom']?></th>
                     <th scope="col"><?=$v['pseudo']?></th>
-                    <th scope="col">
-                        <div class=container-fluid>
-                            <form method='post'>
-                                <div class="row">
-                                     <div class="col">
-                      
-                                        <select class="form-select" aria-label="Default select example" name="habilitation[<?=$v['id']?>]">">
-                                        <option selected></option>
-                                        <option value='0'>0</option>
-                                        <option value='1'>1</option>
-                                        <option value='2'>2</option>
-                                        </select>
-                          
-                                    </div>
-                                    <div class="col">
-                                    <button type='submit' id="ok" name='ok' class="btn btn-success btn-sm">ok</button>
-                                    </div>
-                                </div>
-                            </form> 
-                        </div>
-                    </th>
-                    <th scope="col"><?=$v['date_inscription']?></th>
-                    <th scope="col"><?=$v['date_derniere_visite']?></th>
-                    <th scope="col"><?=$v['date_dernier_creneau']?></th>
+                    <th scope="col"><?=$v['Heuretotal']?></th>
+                
                 </tr>
                 <?php              
                 endforeach;
@@ -84,26 +57,8 @@ $listOfUsers = $admin->getAllUsersAndDateWaiting();
           </table>
       </form>
   </div>
-
+  
 <?php
-
-//fonction qui va permettre de renvoyer le total d'heure de benevolat
-
-/* public function getNbtotalbenevolat(){
-
-  $sql = 'SELECT start,end , id_user, SUM  AS nb_total_heure
-    FROM EVENTS,INSCRIPTION_CRENEAU
-    INNER JOIN id.event.INSCRIPTION_CRENEAU=EVENTS.id
-    GROUP BY  
-    
-    $sth = $this -> pdo -> query($sql);
-    $result = $sth -> fetchAll();
-    
-    return $result;
-}*/
-
-
-
 
 else:
   echo 'Vous n\'êtes pas administrateur, vous n\'avez pas accès à cette page, merci.';
@@ -111,10 +66,3 @@ endif;
 
 include('../includes/footer.php');
 ?>
-
-
-
-
-
-<?php
-
