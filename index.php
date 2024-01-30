c6a2d4d4719951b5e0ec27e4565f6865d209a384
SebV77170 <sebastienvoillot@gmail.com>
2024-01-30 11:58:37 +0100

    Version à jour Fev24

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 if($_SERVER["HTTP_HOST"]=='localhost:8888'):
-    header('location:/public/index.php');
+    header('location:http://localhost:8888/benevoles-ressource-brie/public/index.php');
 else:
     header('location:http://benevoles.ressourcebrie.fr/public/');
 endif;
