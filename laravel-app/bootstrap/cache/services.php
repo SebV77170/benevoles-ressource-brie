147b146064bb70bd4f2401b70e5639c1f123ad76
Sebastien Voillot <sebastienvoillot@gmail.com>
2026-03-22 02:19:51 +0100

    Modif pour faire fonctionner laravel

--- laravel-app/bootstrap/cache/services.php
+++ laravel-app/bootstrap/cache/services.php
@@ -28,7 +28,8 @@
     24 => 'Carbon\\Laravel\\ServiceProvider',
     25 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     26 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    27 => 'App\\Providers\\AuthServiceProvider',
+    27 => 'App\\Providers\\AppServiceProvider',
+    28 => 'App\\Providers\\AuthServiceProvider',
   ),
   'eager' => 
   array (
@@ -45,7 +46,8 @@
     10 => 'Carbon\\Laravel\\ServiceProvider',
     11 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     12 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    13 => 'App\\Providers\\AuthServiceProvider',
+    13 => 'App\\Providers\\AppServiceProvider',
+    14 => 'App\\Providers\\AuthServiceProvider',
   ),
   'deferred' => 
   array (
