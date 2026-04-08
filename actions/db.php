86a3959402b17e074ecd34bf994808538503747a
Sebastien Voillot <sebastienvoillot@gmail.com>
2026-04-08 02:14:53 +0200

    Ajout d'un .env et .env.example

--- actions/db.php
+++ actions/db.php
@@ -1,16 +1,27 @@
 <?php
-            $dbname = /*"brie";*/"objets";
-            $serveur = /*"127.0.0.1";*/"localhost";
-            $login = /*"root";*/ "root";
-            $pass = /*"";*/ "";
-            
-            try{
-                        $db = new PDO("mysql:host=$serveur;dbname=$dbname;charset=utf8;", $login, $pass);
-                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            
-            catch(Exception $e){
-                        die('Une erreur a été trouvée : '.$e->getMessage());
-            }
-  
-        ?>
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+// Charger le .env
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+
+$dotenv->load();
+
+$serveur = $_ENV['DB_HOST'] ?? 'localhost';
+$dbname  = $_ENV['DB_NAME'] ?? '';
+$login   = $_ENV['DB_USER'] ?? '';
+$pass    = $_ENV['DB_PASS'] ?? '';
+$charset = $_ENV['DB_CHARSET'] ?? 'utf8';
+
+try {
+    $dsn = "mysql:host=$serveur;dbname=$dbname;charset=$charset";
+
+    $db = new PDO($dsn, $login, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+
+} catch (Exception $e) {
+    die('Une erreur a été trouvée : ' . $e->getMessage());
+}
