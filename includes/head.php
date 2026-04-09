cb214364f214a7bfe3c6fe1eb42838466e3507de
SebV77170 <124190423+SebV77170@users.noreply.github.com>
2026-04-09 21:19:40 +0200

    adaptation menu

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         <link rel="stylesheet" href="css/calendar.css">
         <link rel="stylesheet" href="css/base.css">
-        <link rel="stylesheet" href="css/nav.css">
+        <link rel="stylesheet" href="css/nav2.css">
         <link rel="preconnect" href="https://fonts.googleapis.com"> 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
         <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Roboto+Condensed&display=swap" rel="stylesheet">
--- includes/nav.php
+++ includes/nav.php
@@ -1,13 +1,40 @@
+<nav class="navbar navbar-expand-lg navforum px-2">
+    <div class="container-fluid">
 
-<nav class="navforum">
-        <ul class="liste">
-                <li <?php if($page == 1){echo 'class="puce vert"';}else{echo 'class="puce bleu"';} ?>><a class="lienpuce" href="calendrier.php">Voir le planning</a></li>
-                <li <?php if($page == 2){echo 'class="puce vert"';}else{echo 'class="puce bleu"';} ?>><a class="lienpuce" href="inscription.php">Prendre un créneau</a></li>
-                <li <?php if($page == 3){echo 'class="puce vert"';}else{echo 'class="puce bleu"';} ?>><a class="lienpuce" href="informations.php">Vos informations</a></li>
-                <li <?php if($page == 5){echo 'class="puce vert"';}else{echo 'class="puce bleu"';} ?>><a class="lienpuce" href="documents.php">Documents</a></li>
-                <?php  if(isset($admin)){?>
-                <li <?php if($page == 4){echo 'class="puce vert"';}else{echo 'class="puce bleu"';} ?>><a class="lienpuce" href="accueil_admin.php">Administration</a></li>
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuForum" aria-controls="menuForum" aria-expanded="false" aria-label="Ouvrir le menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-center show" id="menuForum">
+            <ul class="navbar-nav gap-2 text-center">
+
+                <li class="nav-item">
+                    <a class="nav-link menu-btn <?= ($page == 1) ? 'active-link' : 'inactive-link' ?>" href="calendrier.php">Voir le planning</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-btn <?= ($page == 2) ? 'active-link' : 'inactive-link' ?>" href="inscription.php">Prendre un créneau</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-btn <?= ($page == 3) ? 'active-link' : 'inactive-link' ?>" href="informations.php">Vos informations</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-btn <?= ($page == 5) ? 'active-link' : 'inactive-link' ?>" href="documents.php">Documents</a>
+                </li>
+
+                <?php if(isset($admin)) { ?>
+                <li class="nav-item">
+                    <a class="nav-link menu-btn <?= ($page == 4) ? 'active-link' : 'inactive-link' ?>" href="accueil_admin.php">Administration</a>
+                </li>
                 <?php } ?>
-                <li class="puce bleu"><a class="lienpuce" href="logout.php">Logout</a></li>     
-        </ul>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-btn inactive-link" href="logout.php">Logout</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
 </nav>
