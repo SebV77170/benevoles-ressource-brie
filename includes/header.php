7277b11905109093f2a5c2b796190463f5af8efc
SebV77170 <124190423+SebV77170@users.noreply.github.com>
2026-04-09 21:35:27 +0200

    idem

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header class="entete">
     
-    <a class= "lien_entete" href="index.php"><img class="logo" src="../image/logorondredim.gif" alt="le logo de l'association" ></a>
+    <a class= "lien_entete" href="index.php"><img class="logo" src="/image/logorondredim.gif" alt="le logo de l'association" ></a>
     <?php
         echo '<h1 class="titre">'.$titre.'</h1>';
     ?> 
