3c92cc3fbb0cbfa8edd3bce1cb9594d73e376a97
Sebastien Voillot <sebastienvoillot@gmail.com>
2026-04-13 23:14:06 +0200

    nouvelle modif

--- includes/head.php
+++ includes/head.php
@@ -3,9 +3,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-        <link rel="stylesheet" href="css/calendar.css">
-        <link rel="stylesheet" href="css/base.css">
-        <link rel="stylesheet" href="css/nav2.css">
+        <link rel="stylesheet" href="css/calendar.css?v=<?= filemtime('css/calendar.css') ?>">
+        <link rel="stylesheet" href="css/base.css?v=<?= filemtime('css/base.css') ?>">
+        <link rel="stylesheet" href="css/nav2.css?v=<?= filemtime('css/nav2.css') ?>">
         <link rel="preconnect" href="https://fonts.googleapis.com"> 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
         <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Roboto+Condensed&display=swap" rel="stylesheet">
